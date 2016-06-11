56b7d81ae60bc7c8916e99188a48976ec627e630
Matt Mazzola <mattmazzola@live.com>
2016-06-11 00:37:09 -0700

    Update included hook to include bower file and shim

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ module.exports = {
 
   isDevelopingAddon: function() {
     return true;
+  },
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+    
+    app.import(app.bowerDirectory + '/fuzzy/lib/fuzzy.js');
+    app.import('vendor/shims/fuzzy.js');
   }
 };
